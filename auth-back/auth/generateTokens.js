ebd31699a820a3bc9eb07895d6180f71ec2dacd3
Fatima Chavez <fatimachavezmoron@gmail.com>
2023-07-27 18:50:19 +0200

    add changes to auth folder

--- auth-back/auth/generateTokens.js
+++ auth-back/auth/generateTokens.js
@@ -1,21 +1,26 @@
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 function sign(payload, isAccessToken) {
-  return jwt.sign(payload, isAccessToken
-     ? process.env.ACCESS_TOKEN_SECRET 
-     : process.env.REFRESH_TOKEN_SECRET,
-     {
-        algorithm: "HS256",
-        expiresIn: 3600,
-     }
+  console.log("payload", payload);
+  return jwt.sign(
+    payload,
+    isAccessToken
+      ? process.env.ACCESS_TOKEN_SECRET
+      : process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: 3600,
+      algorithm: "HS256",
+    }
   );
-};
+}
 
-function generateAccessToken(user){
-  return sign({user}, true);
-};
-function generateRefreshToken(user){
-  return sign({user}, false);
-};
+// Función para generar un token de acceso utilizando jsonwebtoken
+function generateAccessToken(user) {
+  return sign({ user }, true);
+}
+function generateRefreshToken(user) {
+  return sign({ user }, false);
+}
 
-module.exports = { generateAccessToken, generateRefreshToken}
+module.exports = { generateAccessToken, generateRefreshToken };
