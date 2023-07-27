6964926daac8d94c4c6bb2e120e5b4686aaf50d5
Fatima Chavez <fatimachavezmoron@gmail.com>
2023-07-27 18:44:33 +0200

    add changes to getUserInfo

--- auth-back/lib/getUserInfo.js
+++ auth-back/lib/getUserInfo.js
@@ -1,9 +1,9 @@
-function getUserInfo() {
+function getUserInfo(user) {
   return {
     username: user.username,
     name: user.name,
-    id: user.id
-  }
+    id: user.id || user._id,
+  };
 }
 
 module.exports = getUserInfo;
